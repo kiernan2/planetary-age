d04d3ca9a15ab0150d66dd83d6cf1f29b4aa7921
Kiernan2 <kiernan1994@gmail.com>
2021-10-08 14:32:21 -0700

    Added Venus time test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,4 +5,10 @@ describe('dex', () => {
   test('Check Mercury time', () => {
     expect(mercuryTime(15)).toEqual(62);
   })
+
+  test('Check Venus time', () => {
+    expect(VenusTime(15)).toEqual(24);
+  })
+
+
 })
